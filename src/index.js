3cec56688ecf250a46428929ee2874d19c19b71f
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-16 01:30:10 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var Title = require('./app')
+var React = require('react')
+var ReactDOM = require('react-dom')
+
+ReactDOM.render(
+    React.createElement(Title),
+    document.documentElement('root')
+)
